acf536fa73992d013b2ac58b3244ae858fb13ab9
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-23 22:15:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ function theBeatlesPlay(musicians,instruments) {
    return array;
 }
 function johnLennonFacts(array) {
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-var i = 0; 
-while(i < facts.length) {
-  facts[i] = facts[i] + "!!!";
-  i++;}
-  return facts;
+ var i = 0;
+while(i < array.length) {
+  array[i] = array[i] + "!!!";
+  i++;
+  
+}
+  return array;
 }
